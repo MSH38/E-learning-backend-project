a7ea6279abe793721220b73e22187bcc45a4c604
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2022-07-21 18:14:50 +0200

    day1

---
+++ database/migrations/2022_07_21_144336_create_student_exams_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_exams', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->double('mark');
+            $table->foreign('student_id')->references('id')->on('student');
+            $table->foreign('exam_id')->references('id')->on('exam');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_exams');
+    }
+};

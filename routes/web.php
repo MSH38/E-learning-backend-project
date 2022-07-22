c224f63b41087423ff3ea68fc77ba85b73b5c3df
menna12333 <me0950277@gmail.com>
2022-07-23 00:50:28 +0200

    menna

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminsController;
+use App\Http\Controllers\AccountsController;
+use App\Http\Controllers\ParentsController;
+use App\Http\Controllers\InstructorsController;
+use App\Http\Controllers\InstructorsSupportController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +26,57 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
+Route::controller(AdminsController::class)->group(function () {
+    Route::get('admins/create', 'create');
+    Route::get('admins/index', 'index');
+    Route::get('admins/edit/{id}', 'edit');
+    Route::get('admins/update/{id}', 'update');
+    Route::get('admins/delete/{id}', 'delete');
+    
+});
+
+Route::controller(AccountsController::class)->group(function () {
+    Route::get('accounts/create', 'create');
+    Route::get('accounts/index', 'index');
+    Route::get('accounts/edit/{id}', 'edit');
+    Route::get('accounts/update/{id}', 'update');
+    Route::get('accounts/delete/{id}', 'delete');
+    
+});
+
+Route::controller(ParentsController::class)->group(function () {
+    Route::get('parents/create', 'create');
+    Route::get('parents/index', 'index');
+    Route::get('parents/edit/{id}', 'edit');
+    Route::get('parents/update/{id}', 'update');
+    Route::get('parents/delete/{id}', 'delete');
+    
+});
+
+
+
+
+
+Route::controller(InstructorsController::class)->group(function () {
+    Route::get('instructors/create', 'create');
+    Route::get('instructors/index', 'index');
+    Route::get('instructors/edit/{id}', 'edit');
+    Route::get('instructors/update/{id}', 'update');
+    Route::get('instructors/delete/{id}', 'delete');
+    
+});
+
+
+Route::controller(InstructorsSupportController::class)->group(function () {
+    Route::get('instructors_support/create', 'create');
+    Route::get('instructors_support/index', 'index');
+    Route::get('instructors_support/edit/{id}', 'edit');
+    Route::get('instructors_support/update/{id}', 'update');
+    Route::get('instructors_support/delete/{id}', 'delete');
+    
+});
+
+
+
